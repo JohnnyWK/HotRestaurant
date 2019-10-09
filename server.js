2f3b82260cb0dbff0413a9cbb05e951835a784f1
Johnny Kreitzman <kreitzka@gmail.com>
2019-10-09 13:11:51 -0500

    some more js

--- server.js
+++ server.js
@@ -54,20 +54,20 @@ app.get("/api/characters/:character", function(req, res) {
 });
 
 // Create New Characters - takes in JSON input
-app.post("/api/characters", function(req, res) {
+app.post("/api/tables", function(req, res) {
   // req.body hosts is equal to the JSON post sent from the user
   // This works because of our body parsing middleware
-  var newCharacter = req.body;
+  var newCustomer = req.body;
 
   // Using a RegEx Pattern to remove spaces from newCharacter
   // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-  newCharacter.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
+  newCustomer.routeName = newCustomer.name.replace(/\s+/g, "").toLowerCase();
 
-  console.log(newCharacter);
+  console.log(newCustomer);
 
-  characters.push(newCharacter);
+  characters.push(newCustomer);
 
-  res.json(newCharacter);
+  res.json(newCustomer);
 });
 
 // Starts the server to begin listening
