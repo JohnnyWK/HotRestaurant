c1c89b0224854ce259ed43fcef75a546f09e69f2
Johnny Kreitzman <kreitzka@gmail.com>
2019-10-09 14:10:32 -0500

    updated server.js

--- server.js
+++ server.js
@@ -1,77 +1,30 @@
 // Dependencies
 // =============================================================
-var express = require("express");
-var path = require("path");
+const express = require("express");
+const path = require("path");
 
 // Sets up the Express App
 // =============================================================
-var app = express();
-var PORT = process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
+app.listen(PORT, function(){
+    console.log("listening on port 3000")
+})
 
+let reserved = [];
+let waiting = [];
 
-let customers = [
-  {
-    id: "yoda",
-    name: "Yoda",
-    time: "6"
-  },
-];
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname, "/public/index.html"))
+})
 
-// Routes
-// =============================================================
+// ROUTES
+// GET / --> index.html = here's some info about the restaurant
+// GET /reservation ---> reservation.html
+// GET /tables  --> tables
 
-// Basic route that sends the user first to the AJAX Page
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
-
-app.get("/reserve", function(req, res) {
-  res.sendFile(path.join(__dirname, "reserve.html"));
-});
-
-// Displays all characters
-app.get("/api/tables", function(req, res) {
-  return res.json(customers);
-});
-
-// Displays a single character, or returns false
-app.get("/api/tables/:customer", function(req, res) {
-  var chosen = req.params.customer;
-
-  console.log(chosen);
-
-  for (var i = 0; i < customers.length; i++) {
-    if (chosen === customers[i].routeName) {
-      return res.json(customers[i]);
-    }
-  }
-
-  return res.json(false);
-});
-
-// Create New Characters - takes in JSON input
-app.post("/api/tables", function(req, res) {
-  // req.body hosts is equal to the JSON post sent from the user
-  // This works because of our body parsing middleware
-  var newCustomer = req.body;
-
-  // Using a RegEx Pattern to remove spaces from newCharacter
-  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-  newCustomer.routeName = newCustomer.name.replace(/\s+/g, "").toLowerCase();
-
-  console.log(newCustomer);
-
-  characters.push(newCustomer);
-
-  res.json(newCustomer);
-});
-
-// Starts the server to begin listening
-// =============================================================
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
-});
